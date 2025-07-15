d143cc1d647d68a53454d5f306b244e9e1cafdcc
Bane <branimir.vukosavljevic@delmax.rs>
2025-07-15 14:14:26 +0200

    Ispravka dinamičkog učitavanja komponente.

--- resources/views/components/tree/index.blade.php
+++ resources/views/components/tree/index.blade.php
@@ -7,7 +7,7 @@
 <div wire:disabled="updateTree"
     x-ignore
     ax-load
-    ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-tree-component', 'solution-forest/filament-tree') }}"
+    ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-tree-component', 'marcha/filament-tree') }}"
     x-data="treeNestableComponent({
         containerKey: {{ $containerKey }},
         maxDepth: {{ $maxDepth }}
